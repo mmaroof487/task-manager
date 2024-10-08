c5d513a58d888ce5af4840d05da0373a72e8de8c
Mohammad Maroof <mmaroof487@gmail.com>
2024-10-08 21:25:05 +0530

    fix: method

--- index.js
+++ index.js
@@ -18,6 +18,11 @@ app.get("/file/:name", (req, res) => {
   res.render("show", { name: req.params.name, data: data });
  });
 });
+
+app.post("/create", (req, res) => {
+ fs.writeFile(`./files/${req.body.title.split(" ").join("")}.txt`, req.body.details, (err) => {});
+});
+
 app.get("/edit/:name", (req, res) => {
  fs.readFile(`./files/${req.params.name}`, "utf8", (err, data) => {
   res.render("edit", { name: req.params.name, data: data });
@@ -28,10 +33,6 @@ app.post("/edit", (req, res) => {
  fs.rename(`./files/${req.body.old}`, `./files/${req.body.new}.txt`, (err) => {
   res.redirect("/");
  });
-});
-
-app.post("/create", (req, res) => {
- fs.writeFile(`./files/${req.body.title.split(" ").join("")}.txt`, req.body.details, (err) => {});
 });
 
 app.listen(3000);
